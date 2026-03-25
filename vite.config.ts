b1817be8eebc1fb03912c4a32625d63df245007b
Ruacha <ruacha.os114@gmail.com>
2026-03-25 02:40:08 -0400

    1.0 fix url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: '/medisync/',
+  base: '/MediSync/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
